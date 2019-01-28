fa762001d344441a16242c86c3265b7517495bfe
skt-t1-byungi <tiniwz@gmail.com>
2019-01-29 00:19:50 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,4 @@
-import { assert } from './helpers'
-
-type UserOptions = Partial<{
-    maxWidth: number | string,
-    height: number | string,
-    maxDuration: number,
-    hideDuration: number,
-    zIndex: number | string,
-    className: string | string[],
-    color: string
-}>
-
-interface Options {
-    maxWidth: string,
-    height: string,
-    maxDuration: number,
-    hideDuration: number,
-    zIndex: string,
-    className: string,
-    color: string
-}
+import { normalizeOptions, Options, UserOptions } from './helpers'
 
 export class Progress {
     private _el: HTMLDivElement
@@ -130,30 +110,3 @@ export class Progress {
 }
 
 export default Progress
-
-function normalizeOptions (opts: UserOptions): Options {
-    opts = Object.assign({
-        maxWidth: '99.5%',
-        height: '3px',
-        maxDuration: 60000,
-        hideDuration: 400,
-        zIndex: '9999',
-        color: '#ff1a59',
-        className: ''
-    }, opts)
-
-    if (typeof opts.maxWidth === 'number') opts.maxWidth = opts.maxWidth + 'px'
-    if (typeof opts.height === 'number') opts.height = opts.height + 'px'
-    if (typeof opts.zIndex === 'number') opts.zIndex = String(opts.zIndex)
-    if (Array.isArray(opts.className)) opts.className = opts.className.join(' ')
-
-    assert(opts.maxWidth, 'options.maxWidth', 'string')
-    assert(opts.height, 'options.height', 'string')
-    assert(opts.maxDuration, 'options.maxDuration', 'number')
-    assert(opts.hideDuration, 'options.hideDuration', 'number')
-    assert(opts.zIndex, 'options.zIndex', 'string')
-    assert(opts.className, 'options.className', 'string')
-    assert(opts.color, 'options.color', 'string')
-
-    return opts as Options
-}
