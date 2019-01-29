6de2b22631a83d9aef7a33c1431c3ac4334d2341
skt-t1-byungi <tiniwz@gmail.com>
2019-01-29 22:05:27 +0900

    add tests

--- src/test.ts
+++ src/test.ts
@@ -9,7 +9,7 @@ const $ = (selector: string) => document.querySelector(selector)
 const clear = () => (document.body.innerHTML = '')
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
-test('attach dom', t => {
+test('attach element', t => {
     const progress = new Progress({ className: 'bar' })
 
     t.notOk($('.bar'))
@@ -22,12 +22,53 @@ test('attach dom', t => {
 })
 
 test('progress', t => {
-    const progress = new Progress({ className: 'bar' })
+    const progress = new Progress({ className: 'bar', duration: 200, maxWidth: 100 })
+    const getWidth = () => $('.bar').getBoundingClientRect().width
+
     progress.start()
-    t.is($('.bar').getBoundingClientRect().width, 0)
+    t.is(getWidth(), 0)
 
     delay(100).then(() => {
-        t.ok($('.bar').getBoundingClientRect().width > 0)
+        const w = getWidth()
+        t.ok(0 < w && w < 100)
+
+        delay(100).then(() => {
+            t.is(getWidth(), 100)
+
+            delay(50).then(() => {
+                t.is(getWidth(), 100)
+                clear(), t.end()
+            })
+        })
+    })
+})
+
+test('promise', t => {
+    const progress = new Progress({ hideDuration: 0 })
+
+    progress.promise(delay(100))
+    t.ok(progress.isProgress)
+
+    const PERSIST_TIME = 150
+    delay(105 + PERSIST_TIME).then(() => {
+        t.notOk(progress.isProgress)
+        clear(), t.end()
+    })
+})
+
+test('promise # add When added midway', t => {
+    t.plan(3)
+    const progress = new Progress({ hideDuration: 0 })
+
+    progress.promise(delay(100))
+    t.ok(progress.isProgress)
+
+    delay(80).then(() => progress.promise(delay(100)))
+
+    const PERSIST_TIME = 150
+    delay(105 + PERSIST_TIME).then(() => t.ok(progress.isProgress))
+    delay(185 + PERSIST_TIME).then(() => {
+        t.notOk(progress.isProgress)
         clear(), t.end()
     })
 })
