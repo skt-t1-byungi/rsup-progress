c04ded41854c60a1650ed59d86486fcd4e0bcb8f
skt-t1-byungi <tiniwz@gmail.com>
2019-01-29 18:49:00 +0900

    wip

--- src/test.ts
+++ src/test.ts
@@ -5,16 +5,29 @@ declare const puppet: any
 test.onFinish(() => puppet.exit(0));
 (test as any).onFailure(() => puppet.exit(1))
 
-const $ = document.querySelector.bind(document)
+const $ = (selector: string) => document.querySelector(selector)
 const clear = () => (document.body.innerHTML = '')
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
-test('basic', t => {
+test('attach dom', t => {
     const progress = new Progress({ className: 'bar' })
 
     t.notOk($('.bar'))
     progress.start()
     t.ok($('.bar'))
+    progress.end()
+    t.notOk($('.bar'))
 
-    clear()
-    t.end()
+    clear(), t.end()
+})
+
+test('progress', t => {
+    const progress = new Progress({ className: 'bar' })
+    progress.start()
+    t.is($('.bar').getBoundingClientRect().width, 0)
+
+    delay(100).then(() => {
+        t.ok($('.bar').getBoundingClientRect().width > 0)
+        clear(), t.end()
+    })
 })
