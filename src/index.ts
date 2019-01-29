716130e2d653b721eaaa480ded0a6878ae565c17
skt-t1-byungi <tiniwz@gmail.com>
2019-01-29 13:09:37 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-import { normalizeOptions, Options, UserOptions } from './helpers'
+interface UserOptions {
+    maxWidth?: number | string,
+    height?: number | string,
+    duration?: number,
+    hideDuration?: number,
+    zIndex?: number | string,
+    className?: string | string[],
+    color?: string
+}
+
+interface Options {
+    maxWidth: string,
+    height: string,
+    duration: number,
+    hideDuration: number,
+    zIndex: string,
+    className: string,
+    color: string
+}
 
 export class Progress {
     private _el: HTMLDivElement
@@ -14,12 +32,23 @@ export class Progress {
     }
 
     public setOptions (userOpts: UserOptions) {
+        assertProp(userOpts, 'maxWidth', ['number', 'string'])
+        assertProp(userOpts, 'height', ['number', 'string'])
+        assertProp(userOpts, 'duration', 'number')
+        assertProp(userOpts, 'hideDuration', 'number')
+        assertProp(userOpts, 'zIndex', ['number', 'string'])
+        assertProp(userOpts, 'color', 'string')
+
+        if (userOpts.className && (typeof userOpts.className !== 'string' || !Array.isArray(userOpts.className))) {
+            throw new TypeError(`[rsup-progress] Expected \`className\` to be of type "string, string[]".`)
+        }
+
         const opts = this._opts = normalizeOptions(userOpts)
+
         this._el.className = opts.className
         this._css({
             position: 'fixed',
             zIndex: opts.zIndex,
-            opacity: '0',
             top: '0',
             left: '0',
             height: opts.height,
@@ -46,7 +75,7 @@ export class Progress {
         this._css({
             width: '0',
             opacity: '1',
-            transition: `width ${this._opts.maxDuration}ms cubic-bezier(0,1,0,1)`
+            transition: `width ${this._opts.duration}ms cubic-bezier(0,1,0,1)`
         })
 
         document.body.appendChild(this._el)
@@ -83,12 +112,13 @@ export class Progress {
         }
 
         this._isHiding = true
+
         const PERSIST_TIME = 100
 
         this._css({
             width: '100%',
             opacity: '0',
-            transition: `opacity ${this._opts.hideDuration}ms ${PERSIST_TIME}ms`
+            transition: `width 50ms, opacity ${this._opts.hideDuration}ms ${PERSIST_TIME}ms`
         })
 
         setTimeout(() => {
@@ -105,3 +135,35 @@ export class Progress {
 }
 
 export default Progress
+
+function normalizeOptions (opts: UserOptions): Options {
+    opts = Object.assign({
+        maxWidth: '99.5%',
+        height: '3px',
+        duration: 60000,
+        hideDuration: 400,
+        zIndex: '9999',
+        color: '#ff1a59',
+        className: ''
+    }, opts)
+
+    if (typeof opts.maxWidth === 'number') opts.maxWidth = opts.maxWidth + 'px'
+    if (typeof opts.height === 'number') opts.height = opts.height + 'px'
+    if (typeof opts.zIndex === 'number') opts.zIndex = String(opts.zIndex)
+    if (Array.isArray(opts.className)) opts.className = opts.className.join(' ')
+
+    return opts as Options
+}
+
+function assertProp (obj: any, prop: string, expected: string | string[]) {
+    const type = typeof obj[prop]
+    if (type === 'undefined') return
+    if (typeof expected === 'string') expected = [expected]
+    if (expected.indexOf(type) > -1) return
+
+    throw new TypeError(`[rsup-progress] Expected \`${prop}\` to be of type "${expected.join(', ')}", but "${type}".`)
+}
+
+export function delay (ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
